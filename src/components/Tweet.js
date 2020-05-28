f5aa26cc8cb429512188005c7655c4310c74b079
SamuelT12321 <62050366+SamuelT12321@users.noreply.github.com>
2020-05-28 23:40:39 +0800

    Add Tweet UI

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,33 +1,78 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { formatTweet } from '../utils/helpers'
+import { formatTweet, formatDate } from '../utils/helpers'
+import { TiArrowBackOutline } from 'react-icons/ti/index'
+import { TiHeartOutline } from 'react-icons/ti/index'
+import { TiHeartFullOutline } from 'react-icons/ti/index'
 
 class Tweet extends Component {
+		handleLike = (e) =>{
+			e.preventDefault()
+			//todo: handle like twwet
+		}
+
+		toParent = (e, id) => {
+			e.preventDefault()
+			//todo: redirect to praent tweet
+
+		}
 	render(){
+
 		const { tweet } = this.props
 		
 
-		if ( tweet === null ){
-			return <p>This Tweet doesn't existed</p>
-		}
+	    if (tweet === null) {
+	      return <p>This Tweet doesn't existd</p>
+	    }
+	    
 		console.log(this.props)
-		
+
+		const {
+			name, avatar, timestamp, text, hasLinked, likes, replies, id, parent
+		} = tweet
 		return(
 			<div className='tweet'>
+				<img
+					src={avatar}
+					alt={'avatar of ${name}'}
+					className='avatar'
+				/>
 
+				<div className='tweet-info'>
+					<div>
+						<span>{name}</span>
+						<div>{formatDate(timestamp)}</div>
+			            {parent && (
+			              <button className='replying-to' onClick={(e) => this.toParent(e, parent.id)}>
+			                Replying to @{parent.author}
+			              </button>
+			            )}
+						<p>{text}</p>
+					</div>
+					<div className='tweet-icons'>
+						<TiArrowBackOutline className='tweet-icons' />
+						<span>{replies !== 0 && replies}</span>
+						<button className='heart-button' onClick={this.handleLike}>
+							{hasLinked===true
+								? <TiHeartFullOutline color='#e0245e' className='tweet-icon'/>
+								: <TiHeartOutline  className='tweet-icon' />}
+						</button>
+						<span>{likes !== 0 && likes}</span>
+					</div>
+				</div>
 			</div>
 		)
 	}
 }
 
-function mapStateToProps({authedUser,users,tweets}, {id}){
-	const tweet = tweets[id]
-	const parentTweet = tweet ? tweet[tweet.replayingTo] : null
-	return {
-		authedUser,
-		tweet: tweet 
-			? formatTweet(tweet,users[tweet.author],authedUser, parentTweet)
-			: null
+function mapStateToProps ({authedUser, users, tweets}, { id }) {
+  const tweet = tweets[id]
+  const parentTweet = tweet ? tweets[tweet.replyingTo] : null
+return {
+    authedUser,
+    tweet: tweet
+      ? formatTweet(tweet, users[tweet.author], authedUser, parentTweet)
+	  	: null
 	}
 }
 
